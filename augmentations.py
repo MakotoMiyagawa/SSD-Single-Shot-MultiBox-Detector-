eadc4f58e07ed84fb2239d43ef9a8f04d0942e41
Mako <mm581204@gmail.com>
2022-05-07 09:41:56 +0900

    5/7addition

--- augmentations.py
+++ augmentations.py
@@ -41,4 +41,55 @@ class ConvertFromInts(object):
 """
 3.アノテーションデータの正規化を元の状態に戻すクラス
 """
+class ToAbsoluteCoords(object):
+    def __call__(self, image, boxes=None, labels=None):
+        height, width, channels = image.shape
+        boxes[:, 0] *= width
+        boxes[:, 2] *= width
+        boxes[:, 1] *= height
+        boxes[:, 3] *= height
+        
+        return image, boxes, labels
+
+'''
+4. 輝度（明るさ）をランダムに変化させるクラス
+'''
+class RandomBrightness(object):
+    def __init__(Self, delta=32):
+        assert delta >= 0.0
+        assert delta <= 255.0
+        self.delta = delta
+    
+    def __call__(self, image, boxes=None, labels=None):
+        if random.randint(2):
+            delta = tandom.uniform(-self.delta, self.delta)
+            image += delta
+        return image, boxes, labels
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
